0ab91a9fba8276f0440910491ebdb6c9da520e03
Anton Klimansky <antonkl@mail.ru>
2018-05-31 19:57:41 +0300

    Extracted bot name and token.

--- src/main/java/com/mirzalizade/svoyak/bots/HelloBot.java
+++ src/main/java/com/mirzalizade/svoyak/bots/HelloBot.java
@@ -2,7 +2,9 @@ package com.mirzalizade.svoyak.bots;
 
 import com.mirzalizade.svoyak.model.UserMessage;
 import com.mirzalizade.svoyak.repository.UserMessageRepository;
-import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -11,26 +13,31 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
  * @author Anton Klimansky
  */
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class HelloBot extends TelegramLongPollingBot {
 
-    UserMessageRepository userMessageRepository;
+    @Value("${bots.hello.name}")
+    private String name;
+
+    @Value("${bots.hello.key}")
+    private String key;
+
+    @NonNull
+    private UserMessageRepository userMessageRepository;
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getText());
         UserMessage message = new UserMessage(update.getMessage().getText(), update.getMessage().getFrom().getUserName());
         userMessageRepository.save(message);
-
     }
 
     @Override
     public String getBotUsername() {
-        return "MiSvoyak";
+        return name;
     }
 
     @Override
     public String getBotToken() {
-        return "613799774:AAFUJBfLF8CjA21ut-Vt8rYbnKrGU5h31Z8";
+        return key;
     }
 }
